6a7ba87791e33179148f6647545b84248d8a87a4
AnaSBeaa <sanabeatriz953@outlook.com>
2026-03-30 09:22:43 -0300

    Iniciando projeto da Vinharia Agnello

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+let vinho = prompt('Insira o nome do vinho:');
+alert('Vinho cadastrado, veja os detalhes no console.');
+
+let tipo = prompt('Insira o tipo do vinho (tinto, branco, rosé):');
+alert('Tipo do vinho cadastrado, veja os detalhes no console.');
+
+let safra = prompt('Insira a safra do vinho:');
+alert('Safra do vinho cadastrada, veja os detalhes no console.');
+
+let estoque = prompt('Insira a quantidade em estoque:');
+alert('Quantidade em estoque cadastrada, veja os detalhes no console.');
+
+alert('Cadastro realizado, veja os detalhes no console.');
+
+
+console.log('----CADASTRO DE VINHO----');
+console.log(`Vinho: ${vinho}`);
+console.log(`Tipo: ${tipo}`);
+console.log(`Safra: ${safra}`);
+console.log(`Estoque: ${estoque}`);
+
