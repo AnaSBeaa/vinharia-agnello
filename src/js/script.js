022f6f261cb3e7ddfffafe7be7d93c6f1c971fd6
AnaSBeaa <sanabeatriz953@outlook.com>
2026-03-31 16:45:31 -0300

    feat: adiciona controle de valor de custo e venda e exibe dados no console

--- src/js/script.js
+++ src/js/script.js
@@ -10,5 +10,19 @@ alert('Safra do vinho cadastrada, veja os detalhes no console.');
 var estoque = prompt('Insira a quantidade em estoque:');
 alert('Quantidade em estoque cadastrada, veja os detalhes no console.');
 
+var valorCusto = prompt('Insira o valor de custo do vinho:');
+alert('Valor de custo do vinho cadastrado, veja os detalhes no console.');
+
+var valorVenda = prompt('Insira o valor de venda do vinho:');
+alert('Valor de venda do vinho cadastrado, veja os detalhes no console.');
+
 
 alert('Cadastro realizado, veja os detalhes no console.');
+
+console.log('----CADASTRO DE VINHO----');
+console.log(`Vinho: ${vinho}`);
+console.log(`Tipo: ${tipo}`);
+console.log(`Safra: ${safra}`);
+console.log(`Estoque: ${estoque}`);
+console.log(`Valor de Custo: ${valorCusto}`);
+console.log(`Valor de Venda: ${valorVenda}`);
